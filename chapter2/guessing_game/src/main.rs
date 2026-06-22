d04d93b4e5f614ad2b5adda941c60ee6582e81de
Rodrigo Barboni Filho <rodrigo@barboni.dev.br>
2026-06-21 21:51:22 -0300

    feat: geração de numeros aleatorios e estruturas de loop e match

--- chapter2/guessing_game/src/main.rs
+++ chapter2/guessing_game/src/main.rs
@@ -1,16 +1,36 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    println!("The secret number is: {secret_number}");
+    
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
+        
+        // o .read_line retorna o enum Result (Ok or Err)
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line"); // Caso o retorno seja Err
 
-    println!("You guessed: {guess}");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
 
